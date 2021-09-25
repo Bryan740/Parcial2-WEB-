4e981f773464d7f44a7b9c6b25e22c9d075caf2b
Bryan740 <87914070+Bryan740@users.noreply.github.com>
2021-09-25 12:49:04 -0600

    Add files via upload

--- empleados.sql
+++ empleados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-09-2021 a las 10:50:46
+-- Tiempo de generación: 25-09-2021 a las 19:59:46
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -42,9 +42,10 @@ CREATE TABLE `empleados` (
 --
 
 INSERT INTO `empleados` (`id`, `Nombre`, `Apellido`, `Correo`, `Foto`, `created_at`, `updated_at`) VALUES
-(2, 'Bryan Giovanni', 'Sep', 'bryan@gmail.com', 'uploads/4g1sIfmE2T36ECuHaRBsRSIv59u3wbIbwhkkAX9o.jpg', NULL, '2021-09-12 14:34:15'),
-(4, 'Giovanni', 'Corado', 'giovanni@gmail.com', 'uploads/YLBGeXnU9drX62fCHJZnhWCFWDzqTyJ9OoHrgex1.jpg', NULL, NULL),
-(5, 'Bryan Giovanni', 'Corado', 'giovanni@gmail.com', 'uploads/Th9IauLqWMUM8FjHtMEFQqOpbLKHY91J7ptV1tv4.jpg', NULL, NULL);
+(4, 'Giovanni', 'Corado', 'giovanni@gmail.com', 'uploads/YLBGeXnU9drX62fCHJZnhWCFWDzqTyJ9OoHrgex1.jpg', NULL, '2021-09-25 23:56:18'),
+(9, 'Walter', 'Cuc', 'walter@gmail.com', 'uploads/GFOoo4XbOD4rgE1hS8bynlWR9Ee25IxJOqsHKRWT.png', NULL, NULL),
+(10, 'Migdalia', 'Sierra', 'migdalia@gmail.com', 'uploads/RgTBBXR50wnKZQmcUrqRpdwDKFdViRShZowlAW3F.png', NULL, NULL),
+(11, 'William', 'Coy', 'william@gmail.com', 'uploads/jFCAU6TIiT9NZsA8MLAac6cp1qO9wVlRBDpB8tKp.png', NULL, '2021-09-25 23:32:32');
 
 --
 -- Índices para tablas volcadas
@@ -64,7 +65,7 @@ ALTER TABLE `empleados`
 -- AUTO_INCREMENT de la tabla `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
